b4c465587942b2b28c0dbd43d48066b30068adb3
이지호 <jeho1129@gmail.com>
2024-04-08 11:32:09 +0900

    skeleton FE + BE

---
+++ BackEnd/src/main/java/com/ssafy/backend/domain/board/service/BoardService.java
@@ -0,0 +1,4 @@
+package com.ssafy.backend.domain.board.service;
+
+public interface BoardService {
+}
---
+++ BackEnd/src/main/java/com/ssafy/backend/domain/board/service/BoardServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ssafy.backend.domain.board.service;
+
+public class BoardServiceImpl {
+}
