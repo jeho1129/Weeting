9fcc71b2aa4ab307b26244df99d77b24bdd6a065
Seunghye <jeonsh1001@hanyang.ac.kr>
2024-04-22 21:27:33 +0900

    feat: MainPage create

--- FrontEnd/src/pages/login/LoginPage.tsx
+++ FrontEnd/src/pages/login/LoginPage.tsx
@@ -2,11 +2,18 @@ import logo from '@/assets/images/weeting_logo2.png';
 import '../../styles/loginPage.css'
 import Button from '../../components/button/MainBtn';
 import '../../styles/typography.css'
+import { useNavigate } from 'react-router-dom'; // useNavigate 임포트
+
 
 const LoginPage = () => {
+  const navigate = useNavigate();
 
   const handleLoginClick = () => {
     console.log('로그인');
+  };
+
+  const handleMainClick = () => {
+    navigate('/main');
   };
 
   const handleRegisterClick = () => {
@@ -18,6 +25,7 @@ const LoginPage = () => {
       <img src={logo} alt="logo" className="loginImage" />
       <div className='loginButtonContainer FontM'>
         <Button text="로그인" onClick={handleLoginClick} />
+        <Button text="홈으로 가기" onClick={handleMainClick} />
         <Button text="회원가입" onClick={handleRegisterClick} />
       </div>
     </div>
--- FrontEnd/src/pages/status/Router.tsx
+++ FrontEnd/src/pages/status/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from '../login/LoginPage';
+import MainPage from '../main/MainPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
