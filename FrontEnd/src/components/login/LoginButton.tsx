776ee0f967d3c60362b57c6f26a3cbf799010ecd
안준선 <joonsun1006@naver.com>
2024-04-22 17:27:45 +0900

    feat: button styling

---
+++ FrontEnd/src/components/login/LoginButton.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/login/LoginButton.module.css'
+
+const LoginButton = () => {
+  return (
+    <>
+      <button className={styles.btn}>로그인</button>
+    </>
+  );
+};
+
+export default LoginButton;
