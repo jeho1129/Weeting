6672a6145e76ef2ca7db9d25c4e344bdcaf02e48
안준선 <joonsun1006@naver.com>
2024-04-22 15:55:07 +0900

    feat: import logo

--- FrontEnd/src/pages/login/LoginPage.tsx
+++ FrontEnd/src/pages/login/LoginPage.tsx
@@ -1,7 +1,12 @@
+import Logo1 from '@/assets/images/weeting_logo1.png'
+
 const LoginPage = () => {
   return (
-    <h1>I am Login</h1>
-  )
-}
+    <>
+      <h1>I am Login</h1>
+      <img src={Logo1} alt="wettingLogo1" />
+    </>
+  );
+};
 
 export default LoginPage;
