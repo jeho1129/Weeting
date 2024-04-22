b2bab0b9b145076fedc50fc8ae9e16925ff9532e
안준선 <joonsun1006@naver.com>
2024-04-22 16:03:58 +0900

    feat: logo css style

--- FrontEnd/src/pages/login/LoginPage.tsx
+++ FrontEnd/src/pages/login/LoginPage.tsx
@@ -1,10 +1,13 @@
-import Logo1 from '@/assets/images/weeting_logo1.png'
+import Logo1 from '@/assets/images/weeting_logo1.png';
+import styles from '@/styles/login/LoginPage.module.css';
 
 const LoginPage = () => {
   return (
     <>
       <h1>I am Login</h1>
-      <img src={Logo1} alt="wettingLogo1" />
+      <div className={styles.align}>
+        <img src={Logo1} alt="wettingLogo1" />
+      </div>
     </>
   );
 };
