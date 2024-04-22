620c32f587a9fd153523c9568e02cf5efb82e8e8
안준선 <joonsun1006@naver.com>
2024-04-22 16:14:50 +0900

    fix: rename component name

---
+++ FrontEnd/src/pages/login/MainGuestPage.tsx
@@ -0,0 +1,14 @@
+import Logo1 from '@/assets/images/weeting_logo1.png';
+import styles from '@/styles/login/MainGuestPage.module.css';
+
+const MainGuestPage = () => {
+  return (
+    <>
+      <div className={styles.align}>
+        <img src={Logo1} alt="weetingLogo1" />
+      </div>
+    </>
+  );
+};
+
+export default MainGuestPage;
