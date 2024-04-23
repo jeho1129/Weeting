701cfa98057606a1c184fc36a19209683ee85ad5
안준선 <joonsun1006@naver.com>
2024-04-23 09:41:19 +0900

    feat: login styling

--- FrontEnd/src/pages/login/LoginPage.tsx
+++ FrontEnd/src/pages/login/LoginPage.tsx
@@ -2,12 +2,17 @@ import Logo2 from '@/assets/images/Weeting_logo2.png';
 import styles from '@/styles/login/LoginPage.module.css';
 
 const LoginPage = () => {
+
   return (
     <>
       <div className={styles.align}>
         <img src={Logo2} alt="weetingLogo2" />
       </div>
-      
+      <div className={styles.container}>
+        <div className={styles.box}>
+          
+        </div>
+      </div>
     </>
   );
 };
