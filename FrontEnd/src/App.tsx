ebbdc728f18622a049652aa4650351a69ddf54cc
강지수 <pwindx20@gmail.com>
2024-04-23 15:11:29 +0900

    fix: router 수정

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,12 +1,10 @@
 import Router from '@/status/Router';
-import MainGuestFrame from './components/main/MainGuestFrame';
 import '@/styles/typography.css';
 
 function App() {
   return (
     <>
       <Router />
-      <MainGuestFrame />
     </>
   );
 }
