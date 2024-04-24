28637ac36ef2b7dcaddb2f81c9660bcd62c39a2c
전승혜 <jeonsh1001@naver.com>
2024-04-24 15:29:47 +0900

    feat: box setting

--- FrontEnd/src/components/game/GameWaiting.tsx
+++ FrontEnd/src/components/game/GameWaiting.tsx
@@ -1,14 +1,20 @@
 import styles from '@/styles/game/GameWaiting.module.css';
 import GameWaitingReadyButton from '@/components/game/GameWaitingReadyButton';
 import GameWaitingQuitButton from '@/components/game/GameWaitingQuitButton';
+import GameWaitingMemberList from '@/components/game/GameWaitingMemberList';
 
 const GameWaiting = () => {
   return (
     <>
-      <div className={styles.BtnAlign}>
+    <div className={styles.Align}>
+      <div>
+      <GameWaitingMemberList/>
+      </div>
+      <div className={styles.ButtonAlign}>
       <GameWaitingReadyButton />
       <GameWaitingQuitButton />
       </div>
+    </div>
     </>
   );
 };
---
+++ FrontEnd/src/components/game/GameWaitingMemberList.tsx
@@ -0,0 +1,14 @@
+import styles from '@/styles/game/GameWaiting.module.css';
+
+const GameWaitingMemberList = () => {
+  return (
+    <>
+        <div className={styles.Box}>
+            <div>안녕하세용</div>
+            <div><strong>굵은 글씨를 해볼까용</strong></div>
+        </div>
+    </>
+  );
+};
+
+export default GameWaitingMemberList;
