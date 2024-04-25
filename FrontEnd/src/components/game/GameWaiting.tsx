ff12cd819e81b7c2ed5f0f2d8789060328d9d261
전승혜 <jeonsh1001@naver.com>
2024-04-25 09:38:16 +0900

    feat: background building added

--- FrontEnd/src/components/game/GameWaiting.tsx
+++ FrontEnd/src/components/game/GameWaiting.tsx
@@ -3,20 +3,26 @@ import logo from '@/assets/images/logo.png';
 import GameWaitingReadyButton from '@/components/game/GameWaitingReadyButton';
 import GameWaitingQuitButton from '@/components/game/GameWaitingQuitButton';
 import GameWaitingMemberList from '@/components/game/GameWaitingMemberList';
+import GameWaitingChatting from '@/components/game/GameWaitingChatting';
 
 const GameWaiting = () => {
   return (
     <>
-    <div className={styles.Align}>
-      <div>
-      <img className={styles.GameWaitingLogo} src={logo} alt="Logo" />
+    <div className={styles.SpaceBetween}>
+      <div className={styles.Align}>
+        <div>
+        <img className={styles.GameWaitingLogo} src={logo} alt="Logo" />
+        </div>
+        <div>
+        <GameWaitingMemberList/>
+        </div>
+        <div className={styles.ButtonAlign}>
+          <GameWaitingReadyButton />
+          <GameWaitingQuitButton />
+        </div>
       </div>
-      <div>
-      <GameWaitingMemberList/>
-      </div>
-      <div className={styles.ButtonAlign}>
-        <GameWaitingReadyButton />
-        <GameWaitingQuitButton />
+      <div className={styles.ChatAlign}>
+        <GameWaitingChatting/>
       </div>
     </div>
     </>
