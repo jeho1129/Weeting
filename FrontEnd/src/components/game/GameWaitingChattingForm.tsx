c0e08c47fd54fd1d3c3be43cfa6c078b27b17769
전승혜 <jeonsh1001@naver.com>
2024-04-25 14:42:28 +0900

    feat: chattinglist setting

---
+++ FrontEnd/src/components/game/GameWaitingChattingForm.tsx
@@ -0,0 +1,37 @@
+import styles from '@/styles/game/GameWaitingChattingForm.module.css';
+import { useState } from 'react';
+import { getCookie, setCookie } from '@/services/axios';
+
+const GameChattingForm = () => {
+  const [id, setId] = useState('');
+
+  const onChatHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    setCookie('accessToken', 'true', { path: '/' });
+    const accessToken = getCookie('accessToken');
+    console.log('accessToken:', accessToken);
+    console.log('id :', id);
+  };
+  return (
+    <div className={styles.Align}>
+      <form onSubmit={submitHandler}>
+        <div className={styles.Mgb}>
+          <input
+            className={styles.InputBox}
+            id="text"
+            type="text"
+            placeholder="메세지를 입력해주세요"
+            onChange={onChatHandler}
+          />
+          <button className={styles.Btn}>확인</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default GameChattingForm;
---
+++ FrontEnd/src/components/game/GameWaitingChattingList.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { getCookie, setCookie } from '@/services/axios';
+
+const GameChattingList = () => {
+
+  return (
+    <div>
+        채팅리스트
+    </div>
+  );
+};
+
+export default GameChattingList;
