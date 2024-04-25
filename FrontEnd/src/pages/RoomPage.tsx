705abd48a1ab59021bbfd4f6d455f381cb2e0599
안준선 <joonsun1006@naver.com>
2024-04-25 14:16:50 +0900

    feat: dummy room page

---
+++ FrontEnd/src/pages/RoomPage.tsx
@@ -0,0 +1,13 @@
+import MainGuestFrame from '@/components/main/MainGuestFrame';
+import { Outlet } from 'react-router-dom';
+
+const RoomPage = () => {
+  return (
+    <>
+      <Outlet />
+      <MainGuestFrame />;
+    </>
+  );
+};
+
+export default RoomPage;
