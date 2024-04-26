deee02806bb6cb982b7de82cbc2bd468c7189e0a
전승혜 <jeonsh1001@naver.com>
2024-04-26 15:45:56 +0900

    feat: flag location change

--- FrontEnd/src/components/game/GameWaitingLeftSide.tsx
+++ FrontEnd/src/components/game/GameWaitingLeftSide.tsx
@@ -4,17 +4,27 @@ import GameWaitingReadyButton from '@/components/game/GameWaitingReadyButton';
 import GameWaitingQuitButton from '@/components/game/GameWaitingQuitButton';
 import GameWaitingMemberList from '@/components/game/GameWaitingMemberList';
 import GameWaitingLogo from '@/components/game/GameWaitingLogo';
+import GameWordTimer from '@/assets/images/timerNormal.png'
+import GameTimer from '@/assets/images/timerNormal.png'
 
 const GameWaitingLeftSide = ({ roomInfo }: { roomInfo: RoomInfo }) => {
   return (
     <>
     <div className={styles.Align}>
-        <GameWaitingLogo/>  
+        <GameWaitingLogo/>
         <GameWaitingMemberList roommembers={roomInfo.roommembers}/>
-        <div className={styles.ButtonAlign}>
-          <GameWaitingReadyButton roommembers={roomInfo.roommembers}/>
-          <GameWaitingQuitButton roomid={roomInfo.roomid}/>
-        </div>
+        {roomInfo.roomstatus === 'waiting' && (
+          <div className={styles.ButtonAlign}>
+            <GameWaitingReadyButton roommembers={roomInfo.roommembers}/>
+            <GameWaitingQuitButton roomid={roomInfo.roomid}/>
+          </div>
+        )}
+        {/* {roomInfo.roomstatus === 'wordsetting' && (
+          <GameWordTimer/> // 단어 설정 상태일 때 GameWordTimer 렌더링
+        )}
+        {roomInfo.roomstatus === 'start' && (
+          <GameTimer/> // 게임 시작 상태일 때 GameTimer 렌더링
+        )} */}
     </div>
     </>
   );
