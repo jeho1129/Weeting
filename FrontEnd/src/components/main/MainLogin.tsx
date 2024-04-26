8563d78e1323a4b93be36a0065f8092cd0388e49
안준선 <joonsun1006@naver.com>
2024-04-26 15:33:13 +0900

    feat: back route button

--- FrontEnd/src/components/main/MainLogin.tsx
+++ FrontEnd/src/components/main/MainLogin.tsx
@@ -1,10 +1,14 @@
 import logo2 from '@/assets/images/logo2.png';
 import styles from '@/styles/main/MainLogin.module.css';
 import MainLoginForm from './MainLoginForm';
+import HomeButton from '../home/HomeButton';
 
 const MainLogin = () => {
   return (
     <>
+      <div className={styles.ButtonContainer}>
+        <HomeButton {...{ message: '대기', direction: 'back', location: '' }} />
+      </div>
       <div className={styles.Align}>
         <img src={logo2} alt="weetingLogo2" />
       </div>
