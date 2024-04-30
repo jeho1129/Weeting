213f5a4f782657cb889b21faac3634286a59ae75
황인범 <leikra44@gmail.com>
2024-04-30 12:06:54 +0900

    Feat: 외형 조회 시 id 제거 및 이미지 링크 추가, Outfit Enum 이미지 링크, 유저 로그인 리턴값 변경

--- BackEnd/src/main/java/com/ssafy/backend/domain/Outfit/repository/WearingOutfitRepository.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/Outfit/repository/WearingOutfitRepository.java
@@ -1,12 +1,13 @@
 package com.ssafy.backend.domain.Outfit.repository;
 
 import com.ssafy.backend.domain.Outfit.entity.WearingOutfit;
+import com.ssafy.backend.domain.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface WearingOutfitRepository extends JpaRepository<WearingOutfit, Long> {
-    List<WearingOutfit> findByUserID(Long userID);
-    void deleteByUserID(Long userID);
-    void deleteByUserIDAndPart(Long userID, String part);
+    List<WearingOutfit> findByUser_Id(Long userID);
+    void deleteByUser_IdAndPart(Long userId, String part);
+
 }
--- BackEnd/src/main/java/com/ssafy/backend/domain/security/service/AuthService.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/security/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.domain.security.service;
 
 import com.ssafy.backend.domain.security.dto.GeneratedToken;
+import com.ssafy.backend.domain.security.dto.LoginResponse;
 import com.ssafy.backend.domain.security.exception.AuthErrorCode;
 import com.ssafy.backend.domain.security.exception.AuthException;
 import com.ssafy.backend.domain.user.exception.UserErrorCode;
@@ -19,13 +20,16 @@ public class AuthService {
     private final TokenService tokenService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    public GeneratedToken login(String account, String password){
-        User user = userRepository.findByAccount(account).orElseThrow(() -> new UserException(UserErrorCode.NOT_EXISTS_USER));
-        if(passwordEncoder.matches(password, user.getPassword())){
-            return tokenService.generatedToken(user.getId());
-        }
-        throw new AuthException(AuthErrorCode.NOT_EXISTS);
+    public LoginResponse login(String account, String password) {
+        User user = userRepository.findByAccount(account)
+                .orElseThrow(() -> new UserException(UserErrorCode.NOT_EXISTS_USER));
 
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            GeneratedToken generatedToken = tokenService.generatedToken(user.getId());
+            return new LoginResponse(generatedToken.getAccessToken(), user.getId(), user.getNickname(), user.getScore(), user.getRanking());
+        }
+
+        throw new AuthException(AuthErrorCode.NOT_EXISTS);
     }
 
     public void logout(User user){
