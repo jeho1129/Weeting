df909c5ef32de145fb4d3ddcc84fe39fad72e608
안준선 <joonsun1006@naver.com>
2024-04-30 12:08:16 +0900

    feat: login recoil 적용

--- FrontEnd/src/components/main/MainGuest.tsx
+++ FrontEnd/src/components/main/MainGuest.tsx
@@ -1,13 +1,19 @@
 import logo from '@/assets/images/logo.png';
 import MainGuestLoginButton from '@/components/main/MainGuestLoginButton';
 import MainGuestSignupButton from '@/components/main/MainGuestSignupButton';
-import { setCookie } from '@/utils/axios';
 import styles from '@/styles/main/MainGuest.module.css';
+import { getCookie, setCookie } from '@/utils/axios';
+import { useNavigate } from 'react-router-dom';
 
 const MainGuest = () => {
   // 나중에 지우기
   setCookie('accessToken', 'true');
-
+  const navigate = useNavigate();
+  const accessToken = getCookie('accessToken');
+  console.log('accessToken :', accessToken);
+  // if (accessToken === true) {
+  //   navigate('home');
+  // }
   return (
     <>
       <div className={styles.Align}>
