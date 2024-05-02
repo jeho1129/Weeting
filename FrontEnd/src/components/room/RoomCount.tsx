025737b0ee467ca2e4a3cded2d6f62621a00e6e3
안준선 <joonsun1006@naver.com>
2024-05-02 17:14:33 +0900

    feat: room create modal

--- FrontEnd/src/components/room/RoomCount.tsx
+++ FrontEnd/src/components/room/RoomCount.tsx
@@ -1,17 +1,35 @@
-import styles from '@/styles/room/RoomCount.module.css'
+import styles from '@/styles/room/RoomCount.module.css';
 import { CaretLeft, CaretRight } from '@phosphor-icons/react';
 
-const RoomCount = () => {
-  const selectedMaxCount = 4;
+const RoomCount = ({ selectedMaxCount, setSelectedMaxCount }) => {
+  const leftHandler = () => {
+    setSelectedMaxCount((prev: number) => {
+      if (prev === 4) {
+        return 8;
+      }
+      return prev - 2;
+    });
+  };
+
+  const rightHandler = () => {
+    setSelectedMaxCount((prev: number) => {
+      if (prev === 8) {
+        return 4;
+      }
+      return prev + 2;
+    });
+  };
+
   return (
-    <>
-      <div className={styles.Dir}>
-        <CaretLeft size={20} />
+    <div className={styles.RoomCountContainer}>
+      <span className={`${styles.RoomCountLabel} FontM20`}>&#9679; 방 인원</span>
+      <div className={styles.DirContainer}>
+        <CaretLeft size={20} className={styles.Dir} onClick={leftHandler} />
         <div className={styles.MaxCount}>{selectedMaxCount}</div>
-        <CaretRight size={20}/>
+        <CaretRight size={20} className={styles.Dir} onClick={rightHandler} />
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default RoomCount;
