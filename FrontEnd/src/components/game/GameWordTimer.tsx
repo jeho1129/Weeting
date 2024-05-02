25c7deecf31c07e03211655619a8e44dabfcd1a8
전승혜 <jeonsh1001@naver.com>
2024-05-02 11:57:44 +0900

    feat: setting

--- FrontEnd/src/components/game/GameWordTimer.tsx
+++ FrontEnd/src/components/game/GameWordTimer.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import styles from '@/styles/game/GameWordSetting.module.css';
 import timerNormal from '@/assets/images/timerNormal.png';
+import { RoomInfo } from '@/types/game';
 
-const GameWordTimer = ({ roomInfo, changeRoomStatus }) => {
+const GameWordTimer = ({ roomInfo, changeRoomStatus }: {roomInfo : RoomInfo, changeRoomStatus: () =>void}) => {
   // roomStatus가 start일 때 타이머를 240초로 설정
   const initialTime = roomInfo.roomStatus === 'start' ? 240 : 30;
   const [timeLeft, setTimeLeft] = useState(initialTime);
@@ -13,19 +14,14 @@ const GameWordTimer = ({ roomInfo, changeRoomStatus }) => {
         setTimeLeft(timeLeft - 1);
       }, 1000);
       return () => clearTimeout(timerId);
-    } else {
-      if (roomInfo.roomStatus !== 'start') {
-        changeRoomStatus('start');
-      }
+    } else if (timeLeft == 0) {
+        changeRoomStatus();
     }
   }, [timeLeft, roomInfo, changeRoomStatus]);
 
   return (
     <div className={styles.timerContainer}>
-      <p className={styles.timerText}>타이머: {timeLeft}초</p>
-      <div>
-        {/* Timer 구현 */}
-      </div>
+      <p className={styles.timerText}>{timeLeft}초</p>
       <img className={styles.GameWordTimer} src={timerNormal} alt="GameTemplate" />
     </div>
   );
