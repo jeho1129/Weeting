0f42fafb1208de91e94d791483cd6baacb16ee50
전승혜 <jeonsh1001@naver.com>
2024-05-02 14:42:27 +0900

    feat: editing

--- FrontEnd/src/components/game/GameRankModal.tsx
+++ FrontEnd/src/components/game/GameRankModal.tsx
@@ -26,16 +26,15 @@ const GameRankModal: React.FC<GameRankModalProps> = ({ roomInfo, isOpen, onClose
     <div className={`FontM20 ${styles.Container}`}>
       <div className={styles.modal}>
         <ul>
-          {sortedMembers.map((member) => (
-            // 내 금칙어 보여주게
-            <li key={member.userId}>
-              <div>{member.nickname}</div>
-              <div>점수: {member.score}</div>
+          {sortedMembers.map((member, index) => (
+            <li key={member.userId} className={index === 0 ? 'FontM32' : ''}>
+              <div className={styles.Center}>{index === 0 ? ' 1등 ' : ''} {member.nickname}</div>
+              <div className={styles.Center}>점수: {member.score}</div>
             </li>
           ))}
         </ul>
       </div>
-      <button onClick={handleConfirm}>확인</button>
+      <button className={`FontM20 ${styles.Btn}`} onClick={handleConfirm}>확인</button>
     </div>
   );
 };
