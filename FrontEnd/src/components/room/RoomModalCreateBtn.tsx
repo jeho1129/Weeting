f0489ab47cf5b0fbc2a9ac1cd5007f8ad30c576a
안준선 <joonsun1006@naver.com>
2024-05-02 10:04:16 +0900

    feat: room dummy api

--- FrontEnd/src/components/room/RoomModalCreateBtn.tsx
+++ FrontEnd/src/components/room/RoomModalCreateBtn.tsx
@@ -1,30 +1,31 @@
+import { roomCreateApi } from '@/services/roomApi';
+import styles from '@/styles/room/RoomModalCreateBtn.module.css';
+import { XCircle } from '@phosphor-icons/react';
 import { useState } from 'react';
 import Modal from 'react-modal';
-import styles from '@/styles/room/RoomModalCreateBtn.module.css'
-import { XCircle } from "@phosphor-icons/react";
 
 const RoomModalCreateBtn = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const customStyles = {
     overlay: {
-      backgroundColor: " rgba(0, 0, 0, 0.4)",
+      backgroundColor: ' rgba(0, 0, 0, 0.4)',
     },
     content: {
       position: 'absolute',
-      width: "500px",
-      height: "270px",
+      width: '500px',
+      height: '270px',
       top: '50%',
       left: '50%',
-      transform: "translate(-50%, -50%)",
+      transform: 'translate(-50%, -50%)',
       border: '1px solid #ccc',
-      boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+      boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
       background: 'white',
-      overflow: 'auto',
+      overflow: 'hidden',
       WebkitOverflowScrolling: 'touch',
       borderRadius: '10px',
       outline: 'none',
-      padding: '40px 60px'
-    }
+      padding: '40px 60px',
+    },
   };
 
   const openModal = () => {
@@ -35,31 +36,38 @@ const RoomModalCreateBtn = () => {
     setModalIsOpen(false);
   };
 
+  const createtHandler = () => {
+    console.log('hi');
+    roomCreateApi();
+  };
+
   return (
     <div>
       <button onClick={openModal}>만들기</button>
       <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
-        <div className={styles.Row}>
-          <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 방 이름</span>
-          <input type="text" className={styles.Input} />
+        <XCircle className={styles.XCircle} size={32} onClick={closeModal} />
+        <div className={styles.Container}>
+          <div className={styles.Row}>
+            <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 방 이름</span>
+            <input type="text" className={styles.Input} />
+          </div>
+          <div className={styles.Row}>
+            <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 모드</span>
+            <input type="text" className={styles.Input} />
+          </div>
+          <div className={styles.Row}>
+            <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 방 인원</span>
+            <input type="text" className={styles.Input} />
+          </div>
+          <div className={styles.Row}>
+            <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 비공개방</span>
+            <input type="text" className={styles.Input} />
+          </div>
+          <div className={styles.Row}>
+            <button onClick={createtHandler}>만들기</button>
+            <button>취소하기</button>
+          </div>
         </div>
-        <div className={styles.Row}>
-          <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 모드</span>
-          <input type="text" className={styles.Input} />
-        </div>
-        <div className={styles.Row}>
-          <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 방 인원</span>
-          <input type="text" className={styles.Input} />
-        </div>
-        <div className={styles.Row}>
-          <span className={`${styles.RoomNameLabel} FontM20`}>&#9679; 비공개방</span>
-          <input type="text" className={styles.Input} />
-        </div>
-        <div className={styles.Row}>
-          <button>확인</button>
-          <button>취소</button>
-        </div>
-        <XCircle size={32} onClick={closeModal} />
       </Modal>
     </div>
   );
