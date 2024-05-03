0aee7a8f066fad164f309906c1d046f006c02dab
강지수 <pwindx20@gmail.com>
2024-05-03 17:31:57 +0900

    fix: frame
    수정중

--- FrontEnd/src/status/PrivateRoute.tsx
+++ FrontEnd/src/status/PrivateRoute.tsx
@@ -1,18 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import Frame from '@/components/home/HomeFrame';
 import { getCookie } from '@/utils/axios';
 export default function PrivateRoute() {
   const token: string | undefined = getCookie('accessToken');
 
   if (token) {
-    return (
-      <>
-        <div style={{ width: '100vw', height: '100vh' }}>
-          <Outlet />
-        </div>
-        <Frame />
-      </>
-    );
+    return <Outlet />;
   } else {
     return <Navigate to="/" />;
   }
