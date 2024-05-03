9348c213d82b2b515003d037108be067cd95c126
안준선 <joonsun1006@naver.com>
2024-05-03 17:32:51 +0900

    feat: room styling

--- FrontEnd/src/components/room/RoomList.tsx
+++ FrontEnd/src/components/room/RoomList.tsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { RoomWaitData } from '@/types/roomWaitData';
 import styles from '@/styles/room/RoomList.module.css'
 import watingAvatar from '@/assets/images/inGameAvatar.png'
 import { Lock } from '@phosphor-icons/react';
+import RoomRadioBtn from './RoomRadioBtn';
 
 const RoomList = () => {
   const [roomName, setRoomName] = useState('');
   const [chatRooms, setChatRooms] = useState([]);
+
+  const navigate = useNavigate()
 
   //axios 및 웹소켓 연결되면 풀기
 //   useEffect(() => {
@@ -27,13 +30,17 @@ const RoomList = () => {
 //     }
 //   };
 
+  const asdf = () => {
+    console.log('hi')
+
+  }
   
   return (
     // <div className="container">
       
       <ul className={styles.ListGroup}>
         {RoomWaitData.map((room, index) => (
-          <li key={index} className={styles.OneRoom}>
+          <li key={index} className={styles.OneRoom} onClick={asdf}>
             <div className={`${styles.FirstRow}`}>
               <div className={`${styles.RoomName} FontM32`}>{room.roomName}</div>
               <div className={`${styles.RoomUsers} FontM20`}>{room.roomUsers.length}/{room.roomMaxCnt}</div>
@@ -41,12 +48,16 @@ const RoomList = () => {
               
             </div>
             <div className={styles.SecondRow}>
-              <div>RoomMode</div>
+              <div></div>
+              {room.roomMode === 'rank' ? (
+                <div className={`${styles.Mode} FontM20`}>랭크</div>
+              ) : (
+                <div className={`${styles.Mode} FontM20`}>노말</div>
+              )}
             </div>
-            <div className={styles.Avatar}>
-              <img src={watingAvatar} alt="waitingAvatar" />
+            <div>
+              <img src={watingAvatar} alt="waitingAvatar" className={styles.Avatar} />
             </div>
-            <div>Dummy Image Position Absolute</div>
             <Link to={`/rooms/${room.roomId}`}>
               <h6>{room.roomName} <span className="badge badge-info badge-pill">{room.roomUsers.length}</span></h6>
             </Link>
