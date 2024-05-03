22a30bb78f9322405b10a967f226620ee89922ce
이지호 <jeho1129@naver.com>
2024-05-03 14:10:59 +0900

    feat: ADD Redis Test Code

--- Data/KoreanProcessing/morpheme.py
+++ Data/KoreanProcessing/morpheme.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, APIRouter
+from fastapi import HTTPException, APIRouter, status
 from pydantic import BaseModel
 from pykospacing import Spacing
 from konlpy.tag import Okt
@@ -9,7 +9,7 @@ router = APIRouter()
 spacing = Spacing()
 okt = Okt()
 #################### 해당 코드는 redis 구현 후 주석 처리 해제할 것 #################################
-# 1. 사용자 금지어 입력 시 금지어와 유사 단어 리스트 Redis에 저장하기
+# 1. 사용자 금지어 입력 시 금지어와 유사 단어 + 점수 리스트 Redis에 저장하기
 # 2. 사용자가 채팅 내용 입력하면 형태소 분석하여 단어 리스트로 넘기기
 # 3. 단어 리스트에서 금지어와 유사도 점수가 가장 높은 단어, 점수 Redis에 저장하기
 
@@ -59,14 +59,35 @@ async def process_data(data, forbidden_word):
         }
     }
 
-@router.post("/analyze")
-async def analyze_text(text_data: TextData):
-    user_id = text_data.user_id
-    data = text_data.text
+#################### 해당 코드는 redis 테스트를 위한 임시 API #################################
+# @router.post("/analyze")
+# async def analyze_text(text_data: TextData):
+#     user_id = text_data.user_id
+#     data = text_data.text
     
-    forbidden_word = await redis.get(f"user:{user_id}:forbidden_word")
-    if not forbidden_word:
-        raise HTTPException(status_code=400, detail="Invalid user ID or no forbidden word set for this user.")
+#     forbidden_word = await redis.get(f"user:{user_id}:forbidden_word")
+#     if not forbidden_word:
+#         raise HTTPException(status_code=400, detail="Invalid user ID or no forbidden word set for this user.")
     
-    response = await process_data(data, forbidden_word)
-    return response
+#     response = await process_data(data, forbidden_word)
+#     return response
+
+class ForbiddenWordData(BaseModel):
+    user_id: str
+    forbidden_word: str
+
+@router.post("/forbidden")
+async def store_forbidden_word(data: ForbiddenWordData):
+    user_id = data.user_id
+    forbidden_word = data.forbidden_word
+
+    try:
+        await redis.set(f"user:{user_id}:forbidden_word", forbidden_word)
+        similar_words = await get_similar_words(forbidden_word, 15000)
+        for w in similar_words:
+            await redis.hset(f"similar:{user_id}:{forbidden_word}", w['word'], w['score'])
+
+        return {"message": "Forbidden word and similar words stored successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
