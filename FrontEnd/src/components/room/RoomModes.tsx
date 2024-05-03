c6276b542a05159f3d4642f6588ed421bdee2178
안준선 <joonsun1006@naver.com>
2024-05-03 12:17:53 +0900

    feat: room search

--- FrontEnd/src/components/room/RoomModes.tsx
+++ FrontEnd/src/components/room/RoomModes.tsx
@@ -1,19 +1,22 @@
-import styles from '@/styles/room/RoomModes.module.css'
+import { useEffect, useState } from 'react';
 import RoomWaitRadioBtn from './RoomWaitRadioBtn';
-import { useState } from 'react';
 
 const RoomModes = () => {
   const [roomSelectedMode, setRoomSelectedMode] = useState<number>(0);
-  
+
   const onChangeRoomMode = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRoomSelectedMode(Number(e.target.value));
   };
 
-  return (
-    <>
-      <RoomWaitRadioBtn roomSelectedMode={roomSelectedMode} onChangeRoomMode={onChangeRoomMode} />
-    </>
-  )
-}
+  useEffect(() => {
+    console.log('roomSelectedMode :', roomSelectedMode);
+  }, [roomSelectedMode]);
 
-export default RoomModes
+  return (
+    <div>
+      <RoomWaitRadioBtn roomSelectedMode={roomSelectedMode} onChangeRoomMode={onChangeRoomMode} />
+    </div>
+  );
+};
+
+export default RoomModes;
