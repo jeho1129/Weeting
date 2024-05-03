b2282633881a5f2afb734c436d12936aa0ffe44a
전승혜 <jeonsh1001@naver.com>
2024-05-03 12:12:40 +0900

    feat: dead check

--- FrontEnd/src/components/game/GameWaitingChatting.tsx
+++ FrontEnd/src/components/game/GameWaitingChatting.tsx
@@ -3,23 +3,33 @@ import styles from '@/styles/game/GameWaitingChatting.module.css';
 import GameChattingForm from './GameWaitingChattingForm';
 import GameChattingList from './GameWaitingChattingList';
 import { ChatMessage } from '@/types/chat';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { userState } from '@/recoil/atom';
 
 const GameWaitingChatting: React.FC = () => {
   const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
 
+  const userInfo = useRecoilValue(userState);
+
   const onSendMessage = (message: string) => {
-    const newMessage: ChatMessage = { content: message }; // ChatMessage 타입에 맞게 newMessage 생성
+    const newMessage: ChatMessage = {
+      content: message,
+      time: new Date().toISOString(),
+      nickname: userInfo.nickname, // userInfo에서 nickname을 가져와서 저장
+    };
+    console.log(new Date().toLocaleString());
+
     setChatMessages([...chatMessages, newMessage]); // 새 메시지를 chatMessages 배열에 추가
   };
 
   return (
     <>
-        <div className={styles.ChatBoxBorder}></div>
-        <div className={styles.ChatBox}>
-            <GameChattingList chatMessages={chatMessages}/>
-        </div>
-        <GameChattingForm onSendMessage={onSendMessage}/>
-        <div className={styles.ChatBuilding}></div>
+      <div className={styles.ChatBoxBorder}></div>
+      <div className={styles.ChatBox}>
+        <GameChattingList chatMessages={chatMessages} />
+      </div>
+      <GameChattingForm onSendMessage={onSendMessage} />
+      <div className={styles.ChatBuilding}></div>
     </>
   );
 };
