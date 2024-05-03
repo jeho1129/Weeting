e1d96cc9090e5048b89699d62f57cd6115050111
전승혜 <jeonsh1001@naver.com>
2024-05-03 17:11:51 +0900

    feat: my message

--- FrontEnd/src/components/game/GameWaitingLeftSide.tsx
+++ FrontEnd/src/components/game/GameWaitingLeftSide.tsx
@@ -7,10 +7,18 @@ import GameWaitingReadyButton from '@/components/game/GameWaitingReadyButton';
 import GameWaitingQuitButton from '@/components/game/GameWaitingQuitButton';
 import GameWaitingMemberList from '@/components/game/GameWaitingMemberList';
 import GameWaitingLogo from '@/components/game/GameWaitingLogo';
-import GameWordTimer from '@/components/game/GameWordTimer'
-import GameTimer from '@/assets/images/timerNormal.png'
+import GameWordTimer from '@/components/game/GameWordTimer';
+import GameTimer from '@/assets/images/timerNormal.png';
 
-const GameWaitingLeftSide = ({ roomInfo, scoreUpdates,  changeRoomStatus }: { roomInfo: RoomInfo, scoreUpdates:number; changeRoomStatus: () => void}) => {
+const GameWaitingLeftSide = ({
+  roomInfo,
+  scoreUpdates,
+  changeRoomStatus,
+}: {
+  roomInfo: RoomInfo;
+  scoreUpdates: number;
+  changeRoomStatus: () => void;
+}) => {
   const [blink, setBlink] = useState(false);
 
   useEffect(() => {
@@ -21,27 +29,28 @@ const GameWaitingLeftSide = ({ roomInfo, scoreUpdates,  changeRoomStatus }: { ro
       }, 5000); // 5초 후에 반짝이는 효과를 종료
     }
   }, [roomInfo.roomStatus]);
-    return (
+  return (
     <>
-    <div className={styles.Align}>
-        <GameWaitingLogo/>
-        <GameWaitingMemberList roomStatus={roomInfo.roomStatus} roomMaxCnt={roomInfo.roomMaxCnt} roomUsers={roomInfo.roomUsers}/>
+      <div className={styles.Align}>
+        <GameWaitingLogo />
+        <GameWaitingMemberList
+          roomStatus={roomInfo.roomStatus}
+          roomMaxCnt={roomInfo.roomMaxCnt}
+          roomUsers={roomInfo.roomUsers}
+        />
         {(roomInfo.roomStatus === 'waiting' || roomInfo.roomStatus === 'allready' || roomInfo.roomStatus === 'end') && (
-            <div className={styles.ButtonAlign}>
-              <GameWaitingReadyButton roomUsers={roomInfo.roomUsers} blink={blink} onStartGame={changeRoomStatus}/>
-              <GameWaitingQuitButton roomId={roomInfo.roomId}/>
-            </div>
+          <div className={styles.ButtonAlign}>
+            <GameWaitingReadyButton roomUsers={roomInfo.roomUsers} blink={blink} onStartGame={changeRoomStatus} />
+            <GameWaitingQuitButton roomId={roomInfo.roomId} />
+          </div>
         )}
         {(roomInfo.roomStatus === 'wordsetting' || roomInfo.roomStatus === 'start') && (
-          <GameWordTimer 
-          roomInfo={roomInfo} 
-          changeRoomStatus={changeRoomStatus}
-        />
+          <GameWordTimer roomInfo={roomInfo} changeRoomStatus={changeRoomStatus} />
         )}
         {/* {roomInfo.roomMode === 'rank' && (
           <GameTimer/>
         )} */}
-    </div>
+      </div>
     </>
   );
 };
--- FrontEnd/src/types/chat.ts
+++ FrontEnd/src/types/chat.ts
@@ -1,10 +1,11 @@
 export interface ChatMessage {
-    nickname: string;
-    content: string;
-    time: string;
-  }
-  
-export  interface ScoreUpdate {
-    userId: string;
-    score: number;
-  }
+  userId: number;
+  nickname: string;
+  content: string;
+  time: string;
+}
+
+export interface ScoreUpdate {
+  userId: string;
+  score: number;
+}
