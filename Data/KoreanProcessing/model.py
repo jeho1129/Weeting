26416886bb4f8a91f0a50e0e805709f3c3654a25
이지호 <jeho1129@gmail.com>
2024-05-03 13:39:47 +0900

    fix: Redis 연결 설정 + 코드 리팩토링

--- Data/KoreanProcessing/model.py
+++ Data/KoreanProcessing/model.py
@@ -1,9 +1,14 @@
-from fastapi import HTTPException, APIRouter, status
+from fastapi import HTTPException, APIRouter, status, Query
 from typing import List
 from pydantic import BaseModel
+from pykospacing import Spacing
+from konlpy.tag import Okt
 from model_manager import find_top_similar_words
+import re
 
 router = APIRouter()
+spacing = Spacing()
+okt = Okt()
 
 class SimilarWordsResponse(BaseModel):
     word: str
@@ -20,3 +25,14 @@ async def get_similar_words(input_word, k: int = 10000):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+    
+@router.get("/analyze")
+async def analyze(text: str = Query(None, min_length=1)):
+    try:
+        text = spacing(text)
+        text = re.sub(r'(이와|이의|이가)\b', '', text)
+        morphs = okt.pos(text, norm=True, join=False)
+        filtered_words = [morph for morph, tag in morphs if tag not in ['Josa', 'Suffix']]
+        return filtered_words
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
