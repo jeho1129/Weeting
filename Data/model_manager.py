1023c238acf5b678e9f45d9be4b334bc736c0daf
이지호 <jeho1129@naver.com>
2024-05-03 14:21:48 +0900

    feat: ADD Redis Test Code

--- Data/model_manager.py
+++ Data/model_manager.py
@@ -59,7 +59,7 @@ async def get_similar_words(input_word, k=10000):
         similar_words = find_top_similar_words(input_word, k)
         recalculated_results = [{'word': word, 'score': round(99.98 - idx * 0.02, 2)}
                                 for idx, (score, word) in enumerate(similar_words)]
-        return [result["word"] for result in recalculated_results if result['score'] > 0.001]
+        return recalculated_results
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     except Exception as e:
