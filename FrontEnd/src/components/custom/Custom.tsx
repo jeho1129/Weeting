cb62cd2dd45266e63a835e7ae979fe0f9a2f0bf2
강지수 <pwindx20@gmail.com>
2024-05-03 09:17:38 +0900

    fix: 메인프레임 위치 수정, 아바타 옷 입히기

--- FrontEnd/src/components/custom/Custom.tsx
+++ FrontEnd/src/components/custom/Custom.tsx
@@ -3,27 +3,69 @@ import Avatar from '@/components/avatar/Avatar';
 import HomeButton from '@/components/home/HomeButton';
 import CustomOutfitList from './CustomOutfitList';
 import { useEffect, useState } from 'react';
-import { Outfit } from '@/types/custom';
-import { outfitAllApi } from '@/services/customApi';
-import { useRecoilValue } from 'recoil';
+import { Outfit, OutfitItem } from '@/types/custom';
+import { outfitAllApi, outfitChangeApi, outfitNowApi } from '@/services/customApi';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { userState } from '@/recoil/atom';
+import { userUpdateApi } from '@/services/userApi';
 const Custom = () => {
   const userInfo = useRecoilValue(userState);
+  const setUserInfo = useSetRecoilState(userState);
+  const [nickname, setNickname] = useState(userInfo.nickname);
   const [outfitList, setOutfitList] = useState<Outfit[]>([]);
+  const [nowOutfit, setNowOutfit] = useState<OutfitItem[]>([]);
   useEffect(() => {
+    outfitNowApi(userInfo.userId).then((data: OutfitItem[]) => {
+      setNowOutfit(data);
+      console.log(data);
+    });
     outfitAllApi(userInfo.userId).then((data) => {
       console.log(data);
       setOutfitList(data);
     });
   }, []);
-
   return (
     <>
-      {/* <div className={styles.AvatarContainer}>
-      </div> */}
       <div className={styles.CustomContainer}>
-        <Avatar {...{ move: true, size: 400, isNest: true }} />
-        <CustomOutfitList {...{ outfitList: outfitList }} />
+        <div>
+          <Avatar {...{ move: true, size: 350, isNest: true, outfit: nowOutfit, ingame: true, nickname: nickname }} />
+          <div>
+            <input
+              type="text"
+              value={nickname}
+              maxLength={4}
+              onChange={(e) => {
+                setNickname(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                userUpdateApi(nickname).then(() => {
+                  setUserInfo({
+                    userId: userInfo.userId,
+                    nickname: nickname,
+                    score: userInfo.userId,
+                    ranking: userInfo.userId,
+                  });
+                });
+              }}
+            >
+              수정
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                nowOutfit.forEach((it) => {
+                  outfitChangeApi(userInfo.userId, it.outfitId);
+                });
+              }}
+            >
+              커스텀 확정
+            </button>
+          </div>
+        </div>
+        <CustomOutfitList {...{ outfitList: outfitList, nowOutfit: nowOutfit, setNowOutfit: setNowOutfit }} />
       </div>
       <div className={styles.ButtonContainer}>
         <HomeButton {...{ message: '', direction: 'right', location: 'home' }} />
--- FrontEnd/src/components/custom/CustomOutfitListItem.tsx
+++ FrontEnd/src/components/custom/CustomOutfitListItem.tsx
@@ -4,6 +4,10 @@ import { Outfit } from '@/types/custom';
 const CustomOutfitListItem = ({ outfit }: { outfit: Outfit }) => {
   return (
     <div className={styles.ItemContainer}>
+      <div className={`FontM32 ${outfit.owned ? styles.ItemUnLock : styles.ItemLock}`}>
+        <div className="FontM20">개방조건</div>
+        <div>랭킹 {outfit.getCondition}위</div>
+      </div>
       <img src={outfit.image} alt="" />
       <div>{outfit.name}</div>
     </div>
