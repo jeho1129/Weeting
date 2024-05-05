5f7522d492474b019fdfd85ffe2437a29b0a3640
JaehunJaehun <sirika2547@gmail.com>
2024-05-06 02:29:01 +0900

    feat: websocket connect

--- BackEnd/src/main/java/com/ssafy/backend/global/config/WebSocketConfig.java
+++ BackEnd/src/main/java/com/ssafy/backend/global/config/WebSocketConfig.java
@@ -1,36 +1,38 @@
-//package com.ssafy.backend.global.config;
-//
-//import com.ssafy.backend.global.component.RabbitMqProps;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-//import org.springframework.web.socket.config.annotation.*;
-//
-//@RequiredArgsConstructor
-//@Configuration
-//@EnableWebSocketMessageBroker
-//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-//
-//    private final RabbitMqProps rabbitMqProps;
-//
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.setApplicationDestinationPrefixes("/pub") // pub으로 시작되는 메시지는 브로커로 보내짐, 메시지를 publish하는 경로
-//                .setUserDestinationPrefix("/users") // 특정 사용자에게 메시지 전송시 사용할 주소
-//                .enableStompBrokerRelay("/queue", "/topic", "/exchange","/amq/queue") // subscribe 경로를 설정, 메시지 브로커에서 지원하는 접두사들
-//                .setRelayHost(rabbitMqProps.getHost())
-//                .setVirtualHost("/")
-//                .setRelayPort(61613) // RabbitMQ STOMP 기본 포트
-//                .setSystemLogin(rabbitMqProps.getUsername())
-//                .setSystemPasscode(rabbitMqProps.getPassword())
-//                .setClientLogin(rabbitMqProps.getUsername())
-//                .setClientPasscode(rabbitMqProps.getPassword());
-//    }
-//
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
-//                .withSockJS();
-//    }
-//
-//}
+package com.ssafy.backend.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.backend.domain.chatroom.service.ChatRoomService;
+import com.ssafy.backend.global.component.WebSocketChatRoomHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+
+    private final ChatRoomService chatRoomService;
+
+    public WebSocketConfig(ChatRoomService chatRoomService) {
+        this.chatRoomService = chatRoomService;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketChatRoomHandler(), "/chat")
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public WebSocketChatRoomHandler webSocketChatRoomHandler() {
+        return new WebSocketChatRoomHandler(chatRoomService);
+    }
+
+
+
+}
