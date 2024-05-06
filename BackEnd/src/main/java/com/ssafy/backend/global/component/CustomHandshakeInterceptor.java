4b71d883801fa23d808c793dc36ff51ea36b3815
김재훈 <sirika2547@naver.com>
2024-05-07 08:54:12 +0900

    feat: before test

--- BackEnd/src/main/java/com/ssafy/backend/global/component/CustomHandshakeInterceptor.java
+++ BackEnd/src/main/java/com/ssafy/backend/global/component/CustomHandshakeInterceptor.java
@@ -8,6 +8,9 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 
 import java.util.Map;
 
+/*
+* http 요청에서 인터셉트 해서 사용자의 정보를 웹소켓으로 갖고 간다?
+* */
 public class CustomHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
