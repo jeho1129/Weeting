1a6d823c37812bccb2f7aca4d0a4719b2e953840
김재훈 <sirika2547@naver.com>
2024-05-07 10:37:12 +0900

    feat: room password

--- BackEnd/src/main/java/com/ssafy/backend/domain/chatroom/dto/ChatRoomCreateRequestDto.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/chatroom/dto/ChatRoomCreateRequestDto.java
@@ -10,7 +10,7 @@ import lombok.*;
 public class ChatRoomCreateRequestDto {
 
     private String roomName;  // 방 제목
-    private String roomPassword;  // 비밀번호
+    private int roomPassword;  // 비밀번호
     private int roomMaxCnt;  // 참여가능 최대 유저 수
 
 }
--- BackEnd/src/main/java/com/ssafy/backend/domain/chatroom/dto/ChatRoomDto.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/chatroom/dto/ChatRoomDto.java
@@ -20,7 +20,7 @@ public class ChatRoomDto {
 
     private String roomId;  // 채팅방 고유값
     private String roomName;  // 방 제목
-    private String roomPassword;  // 비밀번호
+    private int roomPassword;  // 비밀번호
     private int roomMaxCnt;  // 참여가능 최대 유저 수
     private List<Long> members;  // 참여 유저 id 목록
     private String theme;  // 대화 주제
