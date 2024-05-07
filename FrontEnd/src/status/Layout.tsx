81fa0afc40314652d734790e7f02d9b1f887dee8
강지수 <pwindx20@gmail.com>
2024-05-07 15:05:12 +0900

    fix: layout 해상도

--- FrontEnd/src/status/Layout.tsx
+++ FrontEnd/src/status/Layout.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Frame from '@/components/home/HomeFrame';
+import { useEffect } from 'react';
+import { getResizeEventListener } from '@/utils/responsiveFrame';
 
 export default function Layout() {
+  useEffect(() => {
+    const FixRatio = getResizeEventListener(1440, 833);
+    window.onresize = FixRatio;
+    FixRatio();
+  }, []);
   return (
     <>
-      <div style={{ width: '100vw', height: '100vh' }}>
-        <Outlet />
+      <div id="outletContainer">
+        <div id="outlet">
+          {/* <div id="outlet" style={{ width: '100vw', height: '100vh' }}> */}
+          <Outlet />
+        </div>
       </div>
       <Frame />
     </>
