78efa643df005fb990d541c2193af5fb71f9337e
김재훈 <sirika2547@naver.com>
2024-05-08 11:33:50 +0900

    feat: 채팅 코드 저장(미완성)

--- BackEnd/src/main/java/com/ssafy/backend/domain/chat/repository/ChatRepository.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/chat/repository/ChatRepository.java
@@ -3,10 +3,7 @@ package com.ssafy.backend.domain.chat.repository;
 import com.ssafy.backend.domain.chat.document.Chat;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 
 public interface ChatRepository extends MongoRepository<Chat, Long> {
-
-    List<Chat> findByRoomId(Long roomId);
 
 }
