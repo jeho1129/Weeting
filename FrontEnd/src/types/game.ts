e2c5574e9ac8450e02b8b5247114cdca9ae0d770
전승혜 <jeonsh1001@naver.com>
2024-05-08 16:25:01 +0900

    feat: websocket setting

--- FrontEnd/src/types/game.ts
+++ FrontEnd/src/types/game.ts
@@ -2,7 +2,7 @@ import { IngameUser } from './user';
 
 export interface RoomInfo {
   roomMode: 'rank' | 'normal';
-  roomId: number;
+  roomId: string;
   roomName: string;
   roomStatus: 'waiting' | 'allready' | 'wordsetting' | 'wordokay' | 'start' | 'end';
   roomForbiddentime: null | string;
