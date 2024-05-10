12d2079d7cd1ba369d7dba283a0fdea2c0403a88
이지호 <jeho1129@naver.com>
2024-05-10 09:41:16 +0900

    fix: 형태소 분석기 Khaiii 설치 Dockerfile 수정

--- Data/KoreanProcessing/morpheme.py
+++ Data/KoreanProcessing/morpheme.py
@@ -2,15 +2,12 @@ from fastapi import HTTPException, APIRouter, status, WebSocket, WebSocketDiscon
 from pydantic import BaseModel
 from pykospacing import Spacing
 from konlpy.tag import Okt
-from khaiii import KhaiiiApi
 from model_manager import get_similar_words
 import aioredis, re, json
 
 router = APIRouter()
 spacing = Spacing()
 okt = Okt()
-
-api = KhaiiiApi()
 
 redis = aioredis.from_url("redis://54.180.158.223:6379", password="c103103", encoding="utf8", decode_responses=True)
 
@@ -29,7 +26,7 @@ async def websocket_endpoint(websocket: WebSocket):
 # 1. 사용자 금지어 입력 시 금지어와 유사 단어 + 점수 리스트 Redis에 저장하기
 # 2. 사용자가 채팅 내용 입력하면 형태소 분석하여 단어 리스트로 넘기기
 # 3. 단어 리스트에서 금지어와 유사도 점수가 가장 높은 단어, 점수 Redis에 저장하기
-# 4. 게임이 끝나면 Redis에 저장되어 있는 유사도 관련 데이터 지우기
+# 4. 게임이 끝나면 Redis에 저장되어 있는 금지어 관련 데이터 지우기
 
 async def check_text_against_forbidden_words(words, user_id):
     forbidden_similar_words = await redis.hgetall(f"similar:{user_id}")
@@ -82,22 +79,12 @@ async def process_message(data):
 
     filtered_data = spacing(chat_content)
     filtered_data = re.sub(r'(이와|이의|이가)\b', '', filtered_data)
-    # morphs = okt.pos(filtered_data, norm=True, join=False)
-    # processed_words = [morph for morph, tag in morphs if tag not in ['Josa', 'Suffix']]
-    # Khaiii를 사용한 형태소 분석
-    morphs = []
-    for word in api.analyze(filtered_data):
-        for morph in word.morphs:
-            if morph.tag not in ['JX', 'EC', 'ETM', 'ETN']:  # 조사와 어미 제거
-                morphs.append(morph.lex)
-
-    processed_words = morphs
+    morphs = okt.pos(filtered_data, norm=True, join=False)
+    processed_words = [morph for morph, tag in morphs if tag not in ['Josa', 'Suffix']]
     most_similar_word, highest_similarity = await check_text_against_forbidden_words(processed_words, user_nickname)
 
     return {
         "nickname": user_nickname,
-        "words": processed_words,
-        "input": data,
         "most_similar_word": most_similar_word,
         "highest_similarity": highest_similarity
     }
