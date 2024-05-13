ca718cf8ca80c6e7934775a714456cec3ebf2065
안준선 <joonsun1006@naver.com>
2024-05-13 11:49:51 +0900

    feat: room websocket 수정

--- FrontEnd/src/components/room/RoomList.tsx
+++ FrontEnd/src/components/room/RoomList.tsx
@@ -1,42 +1,101 @@
 import watingAvatar from '@/assets/images/inGameAvatar.png';
 import roomSign from '@/assets/images/roomSign.png';
+import { userState } from '@/recoil/atom';
 import styles from '@/styles/room/RoomList.module.css';
 import { RoomWaitInfo } from '@/types/room';
 import { Lock } from '@phosphor-icons/react';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { Client } from '@stomp/stompjs';
 import Swal from 'sweetalert2';
+import { ChatMessage } from '@/types/chat';
 
 const RoomList = ({ roomSelectedMode, searchValue }) => {
   const [serverResponseData, setServerResponseData] = useState<RoomWaitInfo>([]);
-  const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+  // const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+
+  const roomId = useParams().id;
+  const userInfo = useRecoilValue(userState);
+  const [stompClient, setStompClient] = useState<Client | null>(null);
+  // 채팅 메세지 리스트
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
 
   const navigate = useNavigate();
 
+  // useEffect(() => {
+  //   // const ws = new WebSocket('wss://54.180.158.223:9002/ws/chatroom/list');
+  //   const ws = new WebSocket('ws://localhost:8080/ws/chatroom/list');
+
+  //   ws.onopen = () => {
+  //     console.log('리스트받아오자아');
+  //   };
+  //   // 서버로부터 메시지를 받는 이벤트 리스너 설정
+  //   ws.onmessage = (event) => {
+  //     const listedData: RoomWaitInfo[] = JSON.parse(event.data);
+  //     setServerResponseData(listedData);
+  //   };
+
+  //   ws.onerror = (error) => {
+  //     console.error('웹소켓 에러 발생:', error);
+  //   };
+
+  //   setWebSocket(ws);
+
+  //   return () => {
+  //     if (ws) {
+  //       ws.close();
+  //     }
+  //   };
+  // }, []);
+
+  // 채팅방 stomp client 연결
   useEffect(() => {
-    const ws = new WebSocket('wss://54.180.158.223:9002/ws/chatroom/list');
+    const client = new Client({
+      brokerURL: `ws://localhost:8080/ws`,
+      // brokerURL: `wss://54.180.158.223:9002/ws`,
+      reconnectDelay: 5000, // 연결 끊겼을 때, 재연결시도까지 지연시간(ms)
+      // connectHeaders: {
+      //   Authorization: `Bearer ${getCookie('accessToken')}`,
+      // },
 
-    ws.onopen = () => {
-      console.log('리스트받아오자아');
-    };
-    // 서버로부터 메시지를 받는 이벤트 리스너 설정
-    ws.onmessage = (event) => {
-      const listedData: RoomWaitInfo[] = JSON.parse(event.data);
-      setServerResponseData(listedData);
-    };
-
-    ws.onerror = (error) => {
-      console.error('웹소켓 에러 발생:', error);
-    };
-
-    setWebSocket(ws);
+      onConnect: () => {
+        console.log('-------웹소캣재훈이랑 연결완료-------');
+        client.subscribe(`/topic/room.all`, (message) => {
+          const roomList: RoomWaitInfo[] = JSON.parse(message.body);
+          setServerResponseData(roomList);
+          console.log("roomList :", roomList)
+        });
+        client.publish({ destination: `/pub/api/v1/chatroom/get/all` });
+      },
+    });
+    client.activate(); // STOMP 클라이언트 활성화
+    setStompClient(client); // STOMP 클라이언트 상태 업데이트
 
     return () => {
-      if (ws) {
-        ws.close();
-      }
+      client.deactivate(); // 컴포넌트 언마운트 시, STOMP 클라이언트 비활성화
     };
   }, []);
+
+  ///////// 함수 선언 //////////////////////////////////////////////////////
+  // 채팅 메세지 보내기
+  // const onSendMessage = () => {
+  //   // const newMessage = {
+  //   //   userId: userInfo.userId,
+  //   //   nickname: userInfo.nickname,
+  //   //   content: content,
+  //   // };
+
+  //   stompClient?.publish({
+  //     destination: `/pub/api/v1/chatroom/get/all`,
+  //     // body: JSON.stringify(newMessage),
+  //   });
+  // };
+  //////////////////////////////////////////////////////////////////////
+
+  // useEffect(() => {
+  //   onSendMessage();
+  // }, []);
 
   useEffect(() => {
     console.log('serverResponseData :', serverResponseData);
