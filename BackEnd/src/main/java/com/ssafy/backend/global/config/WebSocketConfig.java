f5a4b0a9318cc68b923e5c0d6f481fd25bfe94b8
황인범 <leikra44@gmail.com>
2024-05-13 15:37:46 +0900

    FIX:  config 수정

--- BackEnd/src/main/java/com/ssafy/backend/global/config/WebSocketConfig.java
+++ BackEnd/src/main/java/com/ssafy/backend/global/config/WebSocketConfig.java
@@ -61,13 +61,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub") // pub으로 시작되는 메시지는 브로커로 보내짐, 메시지를 publish하는 경로
                 .setUserDestinationPrefix("/users") // 특정 사용자에게 메시지 전송시 사용할 주소
                 .enableStompBrokerRelay("/queue", "/topic", "/exchange","/amq/queue") // subscribe 경로를 설정, 메시지 브로커에서 지원하는 접두사들
-                .setRelayHost(rabbitMqProps.getHost())
+//                .setRelayHost(rabbitMqProps.getHost())
+                .setRelayHost("rabbitmq")
                 .setVirtualHost("/")
                 .setRelayPort(61613) // STOMP 기본 포트
-                .setSystemLogin(rabbitMqProps.getUsername())
-                .setSystemPasscode(rabbitMqProps.getPassword())
-                .setClientLogin(rabbitMqProps.getUsername())
-                .setClientPasscode(rabbitMqProps.getPassword());
+//                .setSystemLogin(rabbitMqProps.getUsername())
+//                .setSystemPasscode(rabbitMqProps.getPassword())
+//                .setClientLogin(rabbitMqProps.getUsername())
+//                .setClientPasscode(rabbitMqProps.getPassword());
+                .setSystemLogin("guest")
+                .setSystemPasscode("guest")
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
 
         log.info(String.format("RabbitMQ Host: %s", rabbitMqProps.getHost()));
         log.info(String.format("RabbitMQ Port: %s", rabbitMqProps.getPort()));
