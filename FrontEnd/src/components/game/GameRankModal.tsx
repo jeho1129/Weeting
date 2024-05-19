3515bd4512027b75e78c8ecd7c4d2643532456b2
전승혜 <jeonsh1001@naver.com>
2024-05-20 05:51:07 +0900

    fix: fix

--- FrontEnd/src/components/game/GameRankModal.tsx
+++ FrontEnd/src/components/game/GameRankModal.tsx
@@ -13,24 +13,18 @@ const GameRankModal = ({ roomInfo, isRankOpen, setRankOpen }: GameRankModalProps
   }
   const sortedMembers =
     roomInfo.roomStatus === 'start' ? [...roomInfo.roomUsers].sort((a, b) => b.score - a.score) : roomInfo.roomUsers;
+  console.log(roomInfo);
+  console.log(sortedMembers);
 
-    return(
-      <>
+  return (
+    <>
       <div className={`FontM20 ${styles.Container}`}>
-      <div className={styles.modal}>
-<div className={styles.RoomUsers}>
+        <div className={styles.modal}>
+          <div className={styles.RoomUsers}>
             {sortedMembers.map((member, index) => (
               <div key={member.id} className={styles.RoomMember}>
-                <div className={styles.Nickname}>
-                {member.nickname}
-                  
-                </div>
-                <div>
-                  {roomInfo.roomStatus === 'end'? (
-                      <span className={styles.Score}>{member.score}</span>
-
-                  ): null}
-                </div>
+                <div className={styles.Nickname}>{member.nickname}</div>
+                <div>{roomInfo.roomStatus === 'end' ? <span className={styles.Score}>{member.score}</span> : null}</div>
                 {/* <div className={styles.Nickname}>
                   {roomInfo.roomStatus === 'wordsetting' ? (
                     <img
@@ -70,52 +64,44 @@ const GameRankModal = ({ roomInfo, isRankOpen, setRankOpen }: GameRankModalProps
               </div>
             ))}
           </div>
-          <div>
-          {/* <button className={`FontM20 ${styles.Btn}`} onClick={() => setRankOpen(false)}>
-         확인
-     </button> */}
-          </div>
-          
+          <div></div>
+        </div>
       </div>
-      
-      </div>
-      
-      </>
-    )
-//   const normalMembers = roomInfo.roomUsers;
-//   const sortedMembers = [...roomInfo.roomUsers].sort((a, b) => b.score - a.score);
+    </>
+  );
+  //   const normalMembers = roomInfo.roomUsers;
+  //   const sortedMembers = [...roomInfo.roomUsers].sort((a, b) => b.score - a.score);
 
-//   return (
-//     <div className={`FontM20 ${styles.Container}`}>
-//       <div className={styles.modal}>
-//         <ul>
-//           {roomInfo.roomMode === 'rank'
-//             ? sortedMembers.map((member, index) => (
-//                 <li key={member.id} className={index === 0 ? 'FontM32' : ''}>
-//                   <div className={styles.Center}>
-//                     {index === 0 ? ' 1등 ' : `${index + 1}등`} {member.nickname}
-//                   </div>
-//                   <div className={styles.Center}>점수: {member.score}</div>
-//                 </li>
-//               ))
-//             : roomInfo.roomMode === 'normal'
-//               ? normalMembers.map((member) => (
-//                   <li key={member.id} className={'FontM32'}>
-//                     <div className={styles.FlexContainer}>
-//                       <div>{member.nickname}</div>
-//                       <div>{member.isAlive === '' ? '생존 😊' : '탈락 🍗'}</div>
-//                     </div>
-//                   </li>
-//                 ))
-//               : null}
-//         </ul>
-//       </div>
-//       <button className={`FontM20 ${styles.Btn}`} onClick={() => setRankOpen(false)}>
-//         확인
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default GameRankModal;
-  }
+  //   return (
+  //     <div className={`FontM20 ${styles.Container}`}>
+  //       <div className={styles.modal}>
+  //         <ul>
+  //           {roomInfo.roomMode === 'rank'
+  //             ? sortedMembers.map((member, index) => (
+  //                 <li key={member.id} className={index === 0 ? 'FontM32' : ''}>
+  //                   <div className={styles.Center}>
+  //                     {index === 0 ? ' 1등 ' : `${index + 1}등`} {member.nickname}
+  //                   </div>
+  //                   <div className={styles.Center}>점수: {member.score}</div>
+  //                 </li>
+  //               ))
+  //             : roomInfo.roomMode === 'normal'
+  //               ? normalMembers.map((member) => (
+  //                   <li key={member.id} className={'FontM32'}>
+  //                     <div className={styles.FlexContainer}>
+  //                       <div>{member.nickname}</div>
+  //                       <div>{member.isAlive === '' ? '생존 😊' : '탈락 🍗'}</div>
+  //                     </div>
+  //                   </li>
+  //                 ))
+  //               : null}
+  //         </ul>
+  //       </div>
+  //       <button className={`FontM20 ${styles.Btn}`} onClick={() => setRankOpen(false)}>
+  //         확인
+  //       </button>
+  //     </div>
+  //   );
+  // };
+};
+export default GameRankModal;
