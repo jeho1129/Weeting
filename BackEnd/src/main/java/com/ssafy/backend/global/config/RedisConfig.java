0b533bf307bfdd46970e518cbbe70955af617edb
김재훈 <sirika2547@naver.com>
2024-05-19 14:37:18 +0900

    feat: theme argument 수정

--- BackEnd/src/main/java/com/ssafy/backend/global/config/RedisConfig.java
+++ BackEnd/src/main/java/com/ssafy/backend/global/config/RedisConfig.java
@@ -55,7 +55,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
 
