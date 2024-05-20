103aa0f8494b3f31bf0645ee5dcc4e896aa6bf46
전승혜 <jeonsh1001@naver.com>
2024-05-20 10:16:56 +0900

    fix

--- FrontEnd/src/components/game/GameRankModal.tsx
+++ FrontEnd/src/components/game/GameRankModal.tsx
@@ -1,45 +1,46 @@
 import styles from '@/styles/game/GameEnd.module.css';
 import { RoomInfo } from '@/types/game';
 import { IngameUser } from '@/types/user';
+import Avatar from '@/components/avatar/Avatar';
 
 interface GameRankModalProps {
   roomInfo: RoomInfo;
-  roomUsersNew: IngameUser;
   isRankOpen: boolean;
   setRankOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const GameRankModal = ({ roomInfo, roomUsersNew, isRankOpen, setRankOpen }: GameRankModalProps) => {
+const GameRankModal = ({ roomInfo, isRankOpen, setRankOpen }: GameRankModalProps) => {
   if (!isRankOpen) {
     return <></>;
   }
-  console.log(roomUsersNew);
-
   return (
     <div className={`FontM20 ${styles.Container}`}>
-      {/* <div className={styles.modal}>
+      <div className={styles.modal}>
         <ul>
-          {roomInfo.roomMode === 'rank'
-            ? roomUsersNew.map((member, index) => (
-                <li key={member.id} className={index === 0 ? 'FontM32' : ''}>
-                  <div className={styles.Center}>
-                    {index === 0 ? ' 1등 ' : `${index + 1}등`} {member.nickname}
-                  </div>
-                  <div className={styles.Center}>점수: {member.score}</div>
-                </li>
-              ))
-            : roomInfo.roomMode === 'normal'
-              ? normalMembers.map((member) => (
-                  <li key={member.id} className={'FontM32'}>
-                    <div className={styles.FlexContainer}>
-                      <div>{member.nickname}</div>
-                      <div>{member.isAlive === '' ? '생존 😊' : '탈락 🍗'}</div>
-                    </div>
-                  </li>
-                ))
-              : null}
+          {roomInfo.roomMode === 'rank' ? (
+            <Avatar
+              {...{
+                userId: roomInfo.roomUsers[0].id,
+                size: 0.6 * 300,
+                location: 'Room',
+                options: {
+                  nickname: roomInfo.roomUsers[0].nickname,
+                  isNest: true,
+                },
+              }}
+            />
+          ) : roomInfo.roomMode === 'normal' ? (
+            roomInfo.roomUsers.map((member) => (
+              <li key={member.id} className={'FontM32'}>
+                <div className={styles.FlexContainer}>
+                  <div>{member.nickname}</div>
+                  <div>{member.isAlive === '' ? '생존 😊' : '탈락 🍗'}</div>
+                </div>
+              </li>
+            ))
+          ) : null}
         </ul>
-      </div> */}
+      </div>
       <button className={`FontM20 ${styles.Btn}`} onClick={() => setRankOpen(false)}>
         확인
       </button>
